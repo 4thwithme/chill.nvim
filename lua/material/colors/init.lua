f42e7c1362e84bce2d38c9cf572c4aa2febb8f0f
4thwithme <aopopenko@gmail.com>
2023-08-23 02:25:23 +0200

    v15

--- lua/material/colors/init.lua
+++ lua/material/colors/init.lua
@@ -151,8 +151,7 @@ elseif vim.g.material_style == "palenight" then
 elseif vim.g.material_style == "deep ocean" then
     -- Deep Ocean theme style
 
-    --colors.editor.bg           = "#0F111A" --
-    colors.editor.bg           = "#111111" --
+    colors.editor.bg           = "#0F111A" --
     colors.editor.bg_alt       = "#090B10"
     colors.editor.fg           = "#5da3e5"
     colors.editor.fg_dark      = "#8F93A2"
